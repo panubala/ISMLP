a60f934df2a6f65037c612931954b7de0b82ba9e
Linus <linusfessler@gmail.com>
2017-03-26 15:52:12 +0200

    ZooSeries

--- src/main/java/ch/ethz/globis/isk/domain/zoodb/ZooSeries.java
+++ src/main/java/ch/ethz/globis/isk/domain/zoodb/ZooSeries.java
@@ -8,41 +8,56 @@ import ch.ethz.globis.isk.domain.Publication;
 import ch.ethz.globis.isk.domain.Series;
 
 public class ZooSeries extends ZooPC implements Series {
-
+	
+	String id;
+	String name;
+	Set<Publication> publications;
+	
+	public ZooSeries() {
+		
+	}
+	
+	public ZooSeries(String id, String name, Set<Publication> publications) {
+		zooActivateWrite();
+		this.id = id;
+		this.name = name;
+		this.publications = publications;
+	}
+	
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return id;
 	}
 
 	@Override
 	public void setId(String id) {
-		// TODO Auto-generated method stub
-
+		zooActivateWrite();
+		this.id = id;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return name;
 	}
 
 	@Override
 	public void setName(String name) {
-		// TODO Auto-generated method stub
-
+		zooActivateWrite();
+		this.name = name;
 	}
 
 	@Override
 	public Set<Publication> getPublications() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return publications;
 	}
 
 	@Override
 	public void setPublications(Set<Publication> publications) {
-		// TODO Auto-generated method stub
-
+		zooActivateWrite();
+		this.publications = publications;
 	}
 
 }
