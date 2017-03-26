66e834ace53c70b8754a703a0fafe13b16f1413a
Linus <linusfessler@gmail.com>
2017-03-26 21:05:24 +0200

    Set IDs

--- src/main/java/ch/ethz/globis/isk/Main.java
+++ src/main/java/ch/ethz/globis/isk/Main.java
@@ -1,8 +1,10 @@
 package ch.ethz.globis.isk;
 
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 
 import org.zoodb.jdo.ZooJdoHelper;
+import org.zoodb.tools.ZooHelper;
 
 import ch.ethz.globis.isk.domain.zoodb.ZooPerson;
 import ch.ethz.globis.isk.domain.zoodb.ZooProceedings;
@@ -16,12 +18,23 @@ import javax.xml.parsers.*;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Main  {
 	
+	static PersistenceManager pm;
+	static final String selectUniqueFrom = "select unique from ch.ethz.globis.isk.domain.zoodb.";
+	
     public static void main(String[] args) {
-    	PersistenceManager pm = ZooJdoHelper.openOrCreateDB("database");
+    	String dbName = "database";
+    	
+        if (ZooHelper.dbExists(dbName)) {
+            ZooHelper.removeDb(dbName);
+        }
+        ZooHelper.createDb(dbName);
+    	pm = ZooJdoHelper.openDB(dbName);
     	
 		try {
 	    	pm.currentTransaction().begin();
@@ -31,13 +44,17 @@ public class Main  {
 	    		pm.makePersistent(publication);
 	    	pm.currentTransaction().commit();
 		} catch (Exception e) {
-			pm.currentTransaction().rollback();
+			e.printStackTrace();
 		} finally {
+	        if (pm.currentTransaction().isActive()) {
+	            pm.currentTransaction().rollback();
+	        }
 	    	pm.close();
+	        pm.getPersistenceManagerFactory().close();
 		}
     }
     
-    private static List<ZooPublication> parseXml() throws Exception {
+    static List<ZooPublication> parseXml() throws Exception {
 		List<ZooPublication> publications = new ArrayList<>();
 		
 		File dblbFiltered = new File("src/main/java/ch/ethz/globis/isk/dblp_filtered.xml");
@@ -53,10 +70,18 @@ public class Main  {
 		while (node != null) {
 			if (node.getNodeType() == Node.ELEMENT_NODE) {
 	            Element element = (Element) node;
+	            String id = element.getAttribute("key");
 	            
+	            ZooPublication publication;
 				switch(element.getTagName()) {
-				case "inproceedings":		publications.add(parseInProceedings(element));	break;
-				case "proceedings":			publications.add(parseProceedings(element));	break;
+				case "inproceedings":		publication = parseInProceedings(element);	break;
+				case "proceedings":			publication = parseProceedings(element);	break;
+				default:					publication = null;
+				}
+				
+				if (publication != null) {
+					publication.setId(id);
+					publications.add(publication);
 				}
 	        }
 			node = node.getNextSibling();
@@ -65,19 +90,29 @@ public class Main  {
 		return publications;
     }
     
-    private static ZooInProceedings parseInProceedings(Element element) {
+    static ZooInProceedings parseInProceedings(Element element) {
     	ZooInProceedings inProceedings = new ZooInProceedings();
     	
 		Node node = element.getFirstChild();
 		while (node != null) {
 			if (node.getNodeType() == Node.ELEMENT_NODE) {
 	            Element child = (Element) node;
+	            
+	            String id = child.getAttribute("key");
+	            String filter = "id=='" + id + "'";
 	            String value = child.getFirstChild().getNodeValue();
 	            
 				switch(child.getTagName()) {
 				case "author":
-					ZooPerson author = new ZooPerson();
-					author.setName(value);
+					Collection<ZooPerson> authors = (Collection<ZooPerson>) pm.newQuery(ZooPerson.class, filter).execute();
+					ZooPerson author;
+					if (authors.isEmpty()) {
+						author = new ZooPerson();
+						author.setId(id);
+						author.setName(value);
+					} else {
+						author = authors.iterator().next();
+					}
 					author.getAuthoredPublications().add(inProceedings);
 					inProceedings.getAuthors().add(author);
 					break;
@@ -98,19 +133,29 @@ public class Main  {
 		return inProceedings;
     }
     
-    private static ZooProceedings parseProceedings(Element element) {
+    static ZooProceedings parseProceedings(Element element) {
     	ZooProceedings proceedings = new ZooProceedings();
     	
 		Node node = element.getFirstChild();
 		while (node != null) {
 			if (node.getNodeType() == Node.ELEMENT_NODE) {
 	            Element child = (Element) node;
+	            
+	            String id = child.getAttribute("key");
+	            String filter = "id=='" + id + "'";
 	            String value = child.getFirstChild().getNodeValue();
 	            
 				switch(child.getTagName()) {
 				case "editor":
-					ZooPerson editor = new ZooPerson();
-					editor.setName(value);
+					Collection<ZooPerson> editors = (Collection<ZooPerson>) pm.newQuery(ZooPerson.class, filter).execute();
+					ZooPerson editor;
+					if (editors.isEmpty()) {
+						editor = new ZooPerson();
+						editor.setId(id);
+						editor.setName(value);
+					} else {
+						editor = editors.iterator().next();
+					}
 					editor.getEditedPublications().add(proceedings);
 					proceedings.getAuthors().add(editor);
 					break;
@@ -118,14 +163,28 @@ public class Main  {
 					proceedings.setTitle(value);
 					break;
 				case "publisher":
-					ZooPublisher publisher = new ZooPublisher();
-					publisher.setName(value);
+					Collection<ZooPublisher> publishers = (Collection<ZooPublisher>) pm.newQuery(ZooPublisher.class, filter).execute();
+					ZooPublisher publisher;
+					if (publishers.isEmpty()) {
+						publisher = new ZooPublisher();
+						publisher.setId(id);
+						publisher.setName(value);
+					} else {
+						publisher = publishers.iterator().next();
+					}
 					publisher.getPublications().add(proceedings);
 					proceedings.setPublisher(publisher);
 					break;
 				case "series":
 					ZooSeries series = new ZooSeries();
-					series.setName(value);
+					Collection<ZooSeries> seriess = (Collection<ZooSeries>) pm.newQuery(ZooSeries.class, filter).execute();
+					if (seriess.isEmpty()) {
+						series = new ZooSeries();
+						series.setId(id);
+						series.setName(value);
+					} else {
+						series = (ZooSeries) seriess.iterator().next();
+					}
 					series.getPublications().add(proceedings);
 					proceedings.setSeries(series);
 					break;
