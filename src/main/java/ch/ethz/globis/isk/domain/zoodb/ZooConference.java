8bee534eaa6384de1bf9c38424806576819458fe
panubala <panuyabalasuntharam@panuyas-mbp.home>
2017-03-26 16:31:39 +0200

    Conference

--- src/main/java/ch/ethz/globis/isk/domain/zoodb/ZooConference.java
+++ src/main/java/ch/ethz/globis/isk/domain/zoodb/ZooConference.java
@@ -8,34 +8,48 @@ import ch.ethz.globis.isk.domain.Conference;
 import ch.ethz.globis.isk.domain.ConferenceEdition;
 
 public class ZooConference extends ZooPC implements Conference {
-
+	
+	String id;
+	String name;
+	Set<ConferenceEdition> editions;
+	
+	public ZooConference(String id, String name, Set<ConferenceEdition> editions){
+		this.id = id;
+		this.name = name;
+		this.editions = editions;
+	}
+	
+	
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return id;
 	}
 
 	public void setId(String id) {
-		// TODO Auto-generated method stub
+		zooActivateWrite();
+		this.id = id;
 
 	}
 
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return name;
 	}
 
 	public void setName(String name) {
-		// TODO Auto-generated method stub
+		zooActivateWrite();
+		this.name = name;
 
 	}
 
 	public Set<ConferenceEdition> getEditions() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return editions;
 	}
 
 	public void setEditions(Set<ConferenceEdition> editions) {
-		// TODO Auto-generated method stub
+		zooActivateWrite();
+		this.editions = editions;
 
 	}
 
