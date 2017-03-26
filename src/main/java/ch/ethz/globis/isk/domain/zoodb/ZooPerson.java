bae33fe90011e3b9dcbdd39ec5fcf2e0c5cb3d5b
Linus <linusfessler@gmail.com>
2017-03-26 15:42:07 +0200

    ZooPerson, ZooProceedings

--- src/main/java/ch/ethz/globis/isk/domain/zoodb/ZooPerson.java
+++ src/main/java/ch/ethz/globis/isk/domain/zoodb/ZooPerson.java
@@ -8,53 +8,70 @@ import ch.ethz.globis.isk.domain.Person;
 import ch.ethz.globis.isk.domain.Publication;
 
 public class ZooPerson extends ZooPC implements Person {
-
+	
+	private String id;
+	private String name;
+	private Set<Publication> authoredPublications;
+	private Set<Publication> editedPublications;
+	
+	public ZooPerson() {
+		
+	}
+	
+	public ZooPerson(String id, String name, Set<Publication> authoredPublications, Set<Publication> editedPublications) {
+		zooActivateWrite();
+		this.id = id;
+		this.name = name;
+		this.authoredPublications = authoredPublications;
+		this.editedPublications = editedPublications;
+	}
+	
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return id;
 	}
 
 	@Override
 	public void setId(String id) {
-		// TODO Auto-generated method stub
-
+		zooActivateWrite();
+		this.id = id;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return name;
 	}
 
 	@Override
 	public void setName(String name) {
-		// TODO Auto-generated method stub
-
+		zooActivateWrite();
+		this.name = name;
 	}
 
 	@Override
 	public Set<Publication> getAuthoredPublications() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return authoredPublications;
 	}
 
 	@Override
 	public void setAuthoredPublications(Set<Publication> authoredPublications) {
-		// TODO Auto-generated method stub
-
+		zooActivateWrite();
+		this.authoredPublications = authoredPublications;
 	}
 
 	@Override
 	public Set<Publication> getEditedPublications() {
-		// TODO Auto-generated method stub
-		return null;
+		zooActivateRead();
+		return editedPublications;
 	}
 
 	@Override
 	public void setEditedPublications(Set<Publication> editedPublications) {
-		// TODO Auto-generated method stub
-
+		zooActivateWrite();
+		this.editedPublications = editedPublications;
 	}
 
 }
---
+++ src/main/java/ch/ethz/globis/isk/domain/zoodb/ZooProceedings.java
@@ -0,0 +1,119 @@
+package ch.ethz.globis.isk.domain.zoodb;
+
+import java.util.Set;
+
+import ch.ethz.globis.isk.domain.ConferenceEdition;
+import ch.ethz.globis.isk.domain.InProceedings;
+import ch.ethz.globis.isk.domain.Proceedings;
+import ch.ethz.globis.isk.domain.Publisher;
+import ch.ethz.globis.isk.domain.Series;
+
+public class ZooProceedings extends ZooPublication implements Proceedings {
+	
+	private String note;
+	private int number;
+	private Publisher publisher;
+	private String volume;
+	private String isbn;
+	private Series series;
+	private ConferenceEdition conferenceEdition;
+	private Set<InProceedings> publications;
+	
+	public ZooProceedings() {
+		
+	}
+	
+	public ZooProceedings(String note, int number, Publisher publisher, String volume, String isbn,
+			Series series, ConferenceEdition conferenceEdition, Set<InProceedings> publications) {
+		zooActivateWrite();
+		this.note = note;
+		this.number = number;
+		this.publisher = publisher;
+		this.volume = volume;
+		this.isbn = isbn;
+		this.series = series;
+		this.conferenceEdition = conferenceEdition;
+		this.publications = publications;
+	}
+	
+	public String getNote() {
+		zooActivateRead();
+		return note;
+	}
+
+	public void setNote(String note) {
+		zooActivateWrite();
+		this.note = note;
+	}
+
+	public int getNumber() {
+		zooActivateRead();
+		return number;
+	}
+
+	public void setNumber(int number) {
+		zooActivateWrite();
+		this.number = number;
+	}
+
+	public Publisher getPublisher() {
+		zooActivateRead();
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		zooActivateWrite();
+		this.publisher = publisher;
+	}
+
+	public String getVolume() {
+		zooActivateRead();
+		return volume;
+	}
+
+	public void setVolume(String volume) {
+		zooActivateWrite();
+		this.volume = volume;
+	}
+
+	public String getIsbn() {
+		zooActivateRead();
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		zooActivateWrite();
+		this.isbn = isbn;
+	}
+
+	public Series getSeries() {
+		zooActivateRead();
+		return series;
+	}
+
+	public void setSeries(Series series) {
+		zooActivateWrite();
+		this.series = series;
+	}
+
+	public ConferenceEdition getConferenceEdition() {
+		zooActivateRead();
+		return conferenceEdition;
+	}
+
+	public void setConferenceEdition(ConferenceEdition conferenceEdition) {
+		zooActivateWrite();
+		this.conferenceEdition = conferenceEdition;
+	}
+
+	public Set<InProceedings> getPublications() {
+		zooActivateRead();
+		return publications;
+	}
+
+	public void setPublications(Set<InProceedings> publications) {
+		zooActivateWrite();
+		this.publications = publications;
+	}
+
+}
