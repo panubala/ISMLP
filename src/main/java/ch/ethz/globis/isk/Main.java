6a6efff92849efe2cc03a2a13e3b02a50b987fb8
Linus <linusfessler@gmail.com>
2017-03-26 13:20:38 +0200

    Eclipse project

---
+++ src/main/java/ch/ethz/globis/isk/Main.java
@@ -0,0 +1,14 @@
+package ch.ethz.globis.isk;
+
+import javax.jdo.PersistenceManager;
+
+import org.zoodb.jdo.ZooJdoHelper;
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Hello World!");
+    	PersistenceManager pm = ZooJdoHelper.openOrCreateDB("database");
+    }
+}
